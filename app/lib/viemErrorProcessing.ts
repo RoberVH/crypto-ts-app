1fa70e8bcfdb3de087add7021c15a85904403bb9
RoberVH <roberto.vicuna@gmail.com>
2024-07-28 12:41:52 -0600

    Corrige diferencias entre modos Dev y Prod de Next 14/Metamask/viem En Dev mode, viemprocessing devuelve  err.constructor.name que tiene el tipo de error corectamente. Sin embargo en Prd devuelve solo 'W' Cambio para devolver err.cause.name solamente que siempre tiene el tipo de error completo que espera la rutina toastError que lo lee de errorMessages.tst De todos modos se salvaguadro cara retorno de este tipo con || 'ErrorIndeterminado' por si aparece una error salvaje no contemplado en el desarrollo

--- app/lib/viemErrorProcessing.ts
+++ app/lib/viemErrorProcessing.ts
@@ -1,39 +1,47 @@
 // Function to process an error returned by a viem library function call
-import { BaseError, ContractFunctionRevertedError, TransactionExecutionError } from 'viem'
-
+import {
+  BaseError,
+  ContractFunctionRevertedError,
+  TransactionExecutionError,
+} from 'viem'
 
 function viemErrorProcessing(err: unknown): string {
   if (err instanceof BaseError) {
     // treatment for  ContractFunctionRevertedError errors
+
     const revertError = err.walk(
       (e) => e instanceof ContractFunctionRevertedError
-    );
+    )
+
     if (revertError instanceof ContractFunctionRevertedError) {
-      const errorName = revertError.data?.errorName ?? '';
-      return errorName || 'ContractFunctionRevertedError';
+      const errorName = revertError.data?.errorName ?? ''
+      return errorName || 'ContractFunctionRevertedError'  //safeguarded it
     }
 
-    // tratment for TransactionExecutionError (for example if user reject transaction in metamask)
+    // treatment for TransactionExecutionError (for example if user reject transaction in metamask)
     if (err instanceof TransactionExecutionError) {
       // Si hay una causa, devolver el nombre de la causa
       if (err.cause instanceof Error) {
-        return err.cause.constructor.name;
+        // err.cause is diferent from err.constructor.name on Production build, and the same in DEV
+        return err.cause.name
       }
       // Si no hay causa, devolver TransactionExecutionError
-      return 'TransactionExecutionError';
+      return 'TransactionExecutionError'
     }
 
     // other  BaseError type of errors
-    return err.constructor.name;
+    //return err.name but safeguarded it;
+    return err.name || 'ErrorIndeterminado'
   }
 
   // if not a BaseError tpye them check for regular error
+
   if (err instanceof Error) {
-    return err.name;
+    return err.name || 'ErrorIndeterminado' // safeguarded it
   }
 
   // everything else failed, tell error couldn't be determined
-  return 'ErrorIndeterminado';
+  return 'ErrorIndeterminado'
 }
 
 export default viemErrorProcessing
